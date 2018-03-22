16a3b037dbbeb72d1844ab6775be553c5644447e
John Cook <cooks.johns@gmail.com>
2018-03-22 09:43:42 -0500

    refactor exercise one

---
+++ js/external.js
@@ -0,0 +1,36 @@
+"use strict";
+
+// 1. InPut
+// 2. Process the input
+// 3. OutPut
+
+// Input
+
+alert("Welcome To My WebSite!");
+
+var userInput = prompt("What is your favorite color?");
+
+alert("My Favorite color is also " + userInput);
+
+// var confirmed = confirmed( 'Are you sure you are older than 18?')
+
+// var name = prompt("Whats your name?");
+
+// var age = prompt("What's your age?"); //  If the user clicks cancel the value returned is null
+
+
+console.log(Number(age) + 10);
+// console.log(parseInt(age) +10);
+
+// console.log(confirmed);  // will be either true or false
+
+
+//Process
+// name = name.toUpperCase();
+
+// var answer = confirm("Do you want to conver your name to uppercase?");
+
+alert(answer);
+
+// OutPut
+alert("Your name is " + name);
