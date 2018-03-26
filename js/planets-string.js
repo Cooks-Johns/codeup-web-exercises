2e130339a0c1c0f124d68e7cfd23da7ed0047a94
John Cook <cooks.johns@gmail.com>
2018-03-26 12:01:59 -0500

    added switch between cards

---
+++ js/planets-string.js
@@ -0,0 +1,55 @@
+(function(){
+    "use strict";
+
+    var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
+
+
+    /**
+     * TODO:
+     * Convert planetsString to an array, and save it in a variable named
+     * planetsArray.
+     * console.log planetsArray to check your work */
+
+     var planetsArray = planetsString.split('|');
+     console.log(planetsArray);
+
+    /**
+     * TODO:
+     * Create a string with <br> tags between each planet. console.log() your
+     * results. Why might this be useful?
+     */
+    console.log(planetsArray.join('<br>'));
+
+    // '<ul>' + planetsArray.join('</li><li');
+    document.write('<ul><li>' + planetsArray.join('</li><li>') + '</li></ul>');
+
+
+
+
+    /* BONUS:
+    * Create another string that would display your planets in an undordered
+    * list. You will need an opening AND closing <ul> tags around the entire
+    * string, and <li> tags around each planet.
+    */
+
+
+    //
+    //  var itemsExist = true;
+    //  var indexNum = 0;
+    // var unorderList = document.createElement('unorderList');
+    // document.getElementById();
+    //
+    // function write_letters() {
+    //     for (i = 0; i ,planetsArray.length; i++) {
+    //     }
+    // }
+    // if (itemsExist){
+    //     write_letters(unorderList);
+    // } else {
+    //     console.log('error!');
+    // }
+
+
+
+
+})();
