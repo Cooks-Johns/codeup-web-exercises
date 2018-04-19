8ceb142a53e73f0a449d94ceffa9fb7d529d7aaa
John <cooks.johns@gmail.com>
2018-04-19 16:34:03 -0500

    start

---
+++ js/promises.js
@@ -0,0 +1,33 @@
+'use strict';
+
+//=========-------------------  PART 1
+
+
+
+
+
+
+
+
+
+
+wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+
+//=========-------------------  PART 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//=========-------------------  PART 3
