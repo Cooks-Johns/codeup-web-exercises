14f327db4a76ec3e90cbfe566c69852ced3cd722
John <cooks.johns@gmail.com>
2018-05-03 21:58:20 -0500

    fixed while loop

--- js/while.js
+++ js/while.js
@@ -5,50 +5,22 @@
 var count = 2;
 while(count <= 65536) {
     console.log(count);
-     count = count * 2;
+    count = count * 2;
 }
 
 var allCones = Math.floor(Math.random() * 50) + 50;
-console.log().floor(Math.random() * 5) + 1;
+do {
+    var conesSold = Math.floor(Math.random() * 5) + 1;
+    if (conesSold <= allCones) {
+        console.log( " I have only sold  " +
+        conesSold + " cones I need to get back out there!");
+        allCones -= conesSold;
+    } else {
+        console.log(" Sorry I cannot sell you " + conesSold +
+            " cones because I only have" + allCones + " cones left.")
+    }
+} while (allCones > 0);
+console.log("Yeah I sold them all, now what?");
 
-function allCones(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-}
-//
+
 // // use for a code that can be ran more than once but uses less code
-// // if there are enough cones
-//      // If there are not enough cones
-//     // If there are no more cones
-// var cones = prompt("How Many cones would you like?");
-// function (cones) {
-//
-// }
-// do {
-//     if (cones === 5)
-//         confirm('5 cones sold...  ');
-//     count++;
-// } while(!answer);
-// console.log('Cannot sell you 6 cones I only have 3... ' + count +
-//     'Yay! I sold them all! ');
-//
-//
-
-
-//
-// for(var i = 3; i<= 10; i++){
-//     //     document.write("This loop is repeating " +i+ " times </br>" );
-//     //
-//     // }
-//
-//     if(i % 4 == 0) {
-//         continue
-//     }
-//     // for(var i = 1; i <= 100; i++) {
-//     if(i == 9) {
-//         break
-//     }
-//
-//     document.write("I have " +i+ " but these " +i+ " aint loops aint one! </br>" )
-// }
