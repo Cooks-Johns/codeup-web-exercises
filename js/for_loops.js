a9f32fbe27aedf884efeb40c8b7f2f325ef9fb9a
John <cooks.johns@gmail.com>
2018-05-03 22:22:51 -0500

    working on 1            22            333            4444            55555            666666            7777777            88888888            999999999

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,45 +1,125 @@
 "use strict";
 
 
+// 7 x 1 = 7  ==================  showMultiplicationTable
+// 7 x 2 = 14
+// 7 x 3 = 21
+// 7 x 4 = 28
+// 7 x 5 = 35
+// 7 x 6 = 42
+// 7 x 7 = 49
+// 7 x 8 = 56
+// 7 x 9 = 63
+// 7 x 10 = 70
 
-function getRndInteger(min, max) {
-     return Math.floor(Math.random() * (max - min) + min);
+
+function showMultipicationTable(number) {
+     for (var i = 1; i <= 10; i++) {
+          console.log(number + " x " + i + " = " + (number * i));
+     }
+
 }
-getRndInteger(1, 9);
+
+showMultipicationTable(7);
+
+
+
 //      ==================   RANDOM NUMBER
+// 123 is odd
+// 80 is even
+// 24 is even
+// 199 is odd
 
-
-// for(var i = 0; i <= 10; i++) {
-// var random = getRndInteger(20, 200);
-//    if(random % 2 === 0) {
-//        console.log(random + " is even!");
-//    } else {
-//        console.log(random + " is odd!");
-//    }
-// }
-
-//  =====================            NESTED FOR LOOP
-
-// function createHalfPyramid () {
-//
-//     for (var i = 0; i < 10 ; i++) {
-//         for (var j = 0; j < i; j++) {
-//             console.log(i);
-//         }
-//     }
-// }
-//
-// createHalfPyramid(9);
-//
-// //
-//=================  FIBONACCI SEQUENCE
-// function fib(num) {
-//     if (num <= 1) return 1;
-//     return fib(num - 1) + fib(num -2);
-// }
-//
-
-
+function randomInt(start, end) {
+     return Math.floor(Math.random() * (end - start)) + start;
 
 }
+function isEven(x) {
+     return x % 2 === 0;
+
+}
+function isOdd(x) {
+     return x % 2 !== 0;
+
+}
+
+var number;
+
+for(var i = 1; i <= 10; i++) {
+     number = randomInt(20, 200);
+     if(isEven(number)){
+          console.log("Hi, coder I'm " + number + " let\'s stay even")
+     }
+     if(isOdd(number)) {
+          console.log("Well HI! I'm " + number +
+              " I might be odd but its not nice to stare!")
+     }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 1     ==================
+// 22
+// 333
+// 4444
+// 55555
+// 666666
+// 7777777
+// 88888888
+// 999999999
+
+
+var string;
+for(var i = 1; i <= 10; i++) {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 100     ==================
+// 95
+// 90
+// 85
+// 80
+// 75
+// 70
+// 65
+// 60
+// 55
+// 50
+// 45
+// 40
+// 35
+// 30
+// 25
+// 20
+// 15
+// 10
+// 5
+
 
